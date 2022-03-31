81c674d12e5e8752308224203432c0fda005e732
Ben Morgan <benrmorgan@gmail.com>
2022-03-30 20:58:30 -0700

    Adding test python file

---
+++ python_challenge.py
@@ -0,0 +1,9 @@
+test_condition1 = 'I love arrays they are my favorite'
+test_condition2 = 'I love days off'
+test_condition3 = 'my favorite season is'
+
+# test_condition3.split(" ")
+def list_maker(test):
+    new_list = test.split(" ")
+    return new_list
+list_maker(test_condition1)
